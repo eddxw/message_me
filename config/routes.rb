4a01d0e74e6cea512ab25fc33c691fb88b84acec
eddxw <eduardo.jemb@gmail.com>
2019-01-04 19:01:09 -0700

    add routes and login routes views controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'login', to: 'sessions#new'
+  get 'chatroom/index'
+  root 'chatroom#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
